f2fa26d572b3a842e8072e321cb852820305720f
Sugavanas <kks21199@gmail.com>
2018-10-22 16:44:51 +0800

    Functions we need.
    Each of them are a screen.

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,48 @@ public class Main {
     {
         Items.addItem(001, "dsd", 15, 6);
         Customers.addCustomer(1, "John", "Doe", "0123456789");
+
+        /**
+         * Main Screen (After launching):
+         * 1)Login
+         * 2)Create staff ( only master password )
+         *
+         * Main Screen:
+         * 1) Inventory Control
+         * 2) Billing
+         * 3) Customers
+         * 4) Edit my profile
+         * 5) Add/Remove staffs (if permission is given show this)
+         *
+         * -Main Screen Inventory Control:
+         * 1) Items
+         * 2) Item Stock
+         *
+         * --Modify Item
+         * 1) Get item details
+         * 2) Add itee
+         * 3) Remove item
+         * 4) Edit item
+         *
+         * --Modify Stock
+         * 1) Edit stock of item
+         * 2) Check stock
+         *
+         * -Billing
+         * 1) Add new bill
+         * 2) Check bill
+         *
+         * -Customers
+         * 1) Add customer
+         * 2) Check customer details
+         * 3) Modify Customer
+         *
+         * -Edit my profile
+         * 1) show information and directly ask for new info (leave blank to keep it uncahnged)
+         *
+         * -Staffs
+         * 1) Add Staff
+         * 2) Remove staff
+         */
     }
 }
